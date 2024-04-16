2fff74c910206c36dd7743663020dae70cd0fbea
PedroTC_02 <ptc00006@red.ujaen.es>
2024-04-16 21:15:46 +0200

    vista Inicio

--- src/main/java/es/ujaen/daw/entregavoluntaria/controller/VehiculosController.java
+++ src/main/java/es/ujaen/daw/entregavoluntaria/controller/VehiculosController.java
@@ -19,88 +19,88 @@ import java.util.logging.Logger;
 @Controller
 @RequestMapping("/vehiculos")
 public class VehiculosController {
-    @Autowired
-    @Qualifier("vehiculoDAOMap")
-    private VehiculosDAO vehiculoDAO;
-
-    private static final Logger logger = Logger.getLogger(VehiculosController.class.getName());
-
-
-    public VehiculosController() {
-
-    }
-
-    @ModelAttribute
-    private void configView(HttpServletRequest request, HttpServletResponse response, ModelMap model) {
-        //Common variables for Views
-        //model.addAttribute("srvUrl", request.getContextPath()+request.getServletPath()+"/clientes");
-        //model.addAttribute("imgUrl", request.getContextPath()+"/images");
-        //model.addAttribute("mediosPago", mediosPago.buscaTodos().toArray());
-    }
-
-    @GetMapping(path = {"","/listado"})
-    public String listado(ModelMap model) {
-        List<Vehiculo> clientes=vehiculoDAO.buscaTodos();
-        model.addAttribute("vehiculos", clientes);
-        logger.info("Listando vehiculos");
-        return "vehiculos/listado_jstl";
-    }
-
-    @GetMapping("/visualiza")
-    public String visualiza(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model ) {
-        model.addAttribute("vehiculo",vehiculoDAO.buscaPorId(id));
-        return "vehiculos/visualiza";
-    }
-
-    @GetMapping("/visualiza/{id}")
-    public String visualizaId(@PathVariable Integer id, ModelMap model ) {
-        model.addAttribute("vehiculo",vehiculoDAO.buscaPorId(id));
-        return "vehiculos/visualiza";
-    }
-
-    @GetMapping("/borra")
-    public String borra(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model) {
-        vehiculoDAO.borrar(id);
-        model.clear();
-        return "redirect:/vehiculos";
-    }
-
-    @GetMapping("/crea")
-    public String creaForm(ModelMap model) {
-        model.addAttribute("cliente",new Vehiculo());
-        return "vehiculos/crea";
-    }
-
-    @PostMapping("/crea")
-    public String crea(@ModelAttribute("vehiculo") @Valid Vehiculo vehiculo, BindingResult result, ModelMap model) {
-        String view="redirect:listado"; //default view
-
-        if (!result.hasErrors()) {
-            vehiculoDAO.crea(vehiculo);
-            model.clear();
-        }else {
-            view="vehiculos/crea"; //ask for correct data
-        }
-        return view;
-    }
-
-    @GetMapping("/edita")
-    public String editaForm(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model ) {
-        model.addAttribute("vehiculo", vehiculoDAO.buscaPorId(id));
-        return "vehiculos/edita";
-    }
-
-    @PostMapping("/edita")
-    public String edita(@ModelAttribute("vehiculo") @Valid Vehiculo vehiculo, BindingResult result, ModelMap model) {
-        String view="redirect:visualiza/";
-        if (!result.hasErrors()) {
-            vehiculoDAO.guarda(vehiculo);
-            model.clear();
-            view=view+vehiculo.getId();
-        } else {
-            view="vehiculos/edita"; //ask for correct data
-        }
-        return view;
-    }
+//    @Autowired
+//    @Qualifier("vehiculoDAOMap")
+//    private VehiculosDAO vehiculoDAO;
+//
+//    private static final Logger logger = Logger.getLogger(VehiculosController.class.getName());
+//
+//
+//    public VehiculosController() {
+//
+//    }
+//
+//    @ModelAttribute
+//    private void configView(HttpServletRequest request, HttpServletResponse response, ModelMap model) {
+//        //Common variables for Views
+//        //model.addAttribute("srvUrl", request.getContextPath()+request.getServletPath()+"/clientes");
+//        //model.addAttribute("imgUrl", request.getContextPath()+"/images");
+//        //model.addAttribute("mediosPago", mediosPago.buscaTodos().toArray());
+//    }
+//
+//    @GetMapping(path = {"","/listado"})
+//    public String listado(ModelMap model) {
+//        List<Vehiculo> clientes=vehiculoDAO.buscaTodos();
+//        model.addAttribute("vehiculos", clientes);
+//        logger.info("Listando vehiculos");
+//        return "vehiculos/listado_jstl";
+//    }
+//
+//    @GetMapping("/visualiza")
+//    public String visualiza(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model ) {
+//        model.addAttribute("vehiculo",vehiculoDAO.buscaPorId(id));
+//        return "vehiculos/visualiza";
+//    }
+//
+//    @GetMapping("/visualiza/{id}")
+//    public String visualizaId(@PathVariable Integer id, ModelMap model ) {
+//        model.addAttribute("vehiculo",vehiculoDAO.buscaPorId(id));
+//        return "vehiculos/visualiza";
+//    }
+//
+//    @GetMapping("/borra")
+//    public String borra(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model) {
+//        vehiculoDAO.borrar(id);
+//        model.clear();
+//        return "redirect:/vehiculos";
+//    }
+//
+//    @GetMapping("/crea")
+//    public String creaForm(ModelMap model) {
+//        model.addAttribute("cliente",new Vehiculo());
+//        return "vehiculos/crea";
+//    }
+//
+//    @PostMapping("/crea")
+//    public String crea(@ModelAttribute("vehiculo") @Valid Vehiculo vehiculo, BindingResult result, ModelMap model) {
+//        String view="redirect:listado"; //default view
+//
+//        if (!result.hasErrors()) {
+//            vehiculoDAO.crea(vehiculo);
+//            model.clear();
+//        }else {
+//            view="vehiculos/crea"; //ask for correct data
+//        }
+//        return view;
+//    }
+//
+//    @GetMapping("/edita")
+//    public String editaForm(@RequestParam(value="id",defaultValue="0")Integer id, ModelMap model ) {
+//        model.addAttribute("vehiculo", vehiculoDAO.buscaPorId(id));
+//        return "vehiculos/edita";
+//    }
+//
+//    @PostMapping("/edita")
+//    public String edita(@ModelAttribute("vehiculo") @Valid Vehiculo vehiculo, BindingResult result, ModelMap model) {
+//        String view="redirect:visualiza/";
+//        if (!result.hasErrors()) {
+//            vehiculoDAO.guarda(vehiculo);
+//            model.clear();
+//            view=view+vehiculo.getId();
+//        } else {
+//            view="vehiculos/edita"; //ask for correct data
+//        }
+//        return view;
+//    }
 
 }
---
+++ src/main/webapp/WEB-INF/tags/header.tag
@@ -0,0 +1,20 @@
+<%@tag description="CitaMotorizada header" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="jakarta.tags.core" %>
+
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <span class="fs-2">
+        <img src="<c:url value='/images/img.png' />" width="90" alt="Logo" />
+        CitaMotorizada
+    </span>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li class="nav-link px-2"><a href="<c:url value="/index.jsp"/>" class="nav-link">Home</a></li>
+        <li><a href="${pageContext.request.contextPath}/Vehiculos/registrarVehiculo" class="nav-link px-2">Registrar vehículo</a></li>
+        <li><a href="${pageContext.request.contextPath}/Vehiculos/vehiculosRegistrados" class="nav-link px-2">Vehículos registrados</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+        <a href="${pageContext.request.contextPath}/loginUsuario" class="btn btn-outline-primary me-2">Inicia Sesión</a>
+        <a href="${pageContext.request.contextPath}/Usuario/registrarUsuario" class="btn btn-primary me-2">Regístrate</a>
+    </div>
+</header>
